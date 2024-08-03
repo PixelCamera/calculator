5047681ce60d2bba16e1c941dc4bfa9ff82c7bd5
PixelCamera <abnerpants@gmail.com>
2024-08-03 17:23:39 +0800

    update Pointer Event

--- src/components/ResponsiveGlowBackground.vue
+++ src/components/ResponsiveGlowBackground.vue
@@ -11,28 +11,14 @@
 </template>
 
 <script setup>
-import { ref, reactive, computed, onMounted, onUnmounted } from 'vue';
+import {ref, reactive, computed, onMounted, onUnmounted} from 'vue';
 
 const containerRef = ref(null);
-const position = reactive({ x: 0, y: 0 });
-const targetPosition = reactive({ x: 0, y: 0 });
+const position = reactive({x: 0, y: 0});
+const targetPosition = reactive({x: 0, y: 0});
 const isActive = ref(false);
-const isMobile = ref(false);
 
-const checkMobile = () => {
-  isMobile.value = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
-};
-
-onMounted(() => {
-  checkMobile();
-  window.addEventListener('resize', checkMobile);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('resize', checkMobile);
-});
-
-const glowSize = computed(() => isMobile.value ? 300 : 600);
+const glowSize = computed(() => 400);
 
 const glowStyle = computed(() => ({
   left: `${position.x}px`,
@@ -50,32 +36,16 @@ const updateTargetPosition = (clientX, clientY) => {
   targetPosition.y = clientY;
 };
 
-const handleTouchStart = (e) => {
+const handlePointerOver = (e) => {
   isActive.value = true;
-  if (e.touches[0]) {
-    updateTargetPosition(e.touches[0].clientX, e.touches[0].clientY);
-  }
-};
-
-const handleTouchMove = (e) => {
-  if (e.touches[0]) {
-    updateTargetPosition(e.touches[0].clientX, e.touches[0].clientY);
-  }
-};
-
-const handleTouchEnd = () => {
-  isActive.value = false;
-};
-
-const handleMouseEnter = () => {
-  isActive.value = true;
-};
-
-const handleMouseMove = (e) => {
   updateTargetPosition(e.clientX, e.clientY);
 };
 
-const handleMouseLeave = () => {
+const handlePointerMove = (e) => {
+  updateTargetPosition(e.clientX, e.clientY);
+};
+
+const handlePointerLeave = () => {
   isActive.value = false;
 };
 
@@ -88,28 +58,16 @@ const animate = () => {
 };
 
 onMounted(() => {
-  if (isMobile.value) {
-    document.addEventListener('touchstart', handleTouchStart, {passive: true});
-    document.addEventListener('touchmove', handleTouchMove, {passive: true});
-    document.addEventListener('touchend', handleTouchEnd);
-  } else {
-    document.addEventListener('mouseenter', handleMouseEnter);
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseleave', handleMouseLeave);
-  }
+  document.addEventListener('pointerover', handlePointerOver);
+  document.addEventListener('pointermove', handlePointerMove);
+  document.addEventListener('pointerleave', handlePointerLeave);
 
   requestAnimationFrame(animate);
 });
 
 onUnmounted(() => {
-  if (isMobile.value) {
-    document.removeEventListener('touchstart', handleTouchStart);
-    document.removeEventListener('touchmove', handleTouchMove);
-    document.removeEventListener('touchend', handleTouchEnd);
-  } else {
-    document.removeEventListener('mouseenter', handleMouseEnter);
-    document.removeEventListener('mousemove', handleMouseMove);
-    document.removeEventListener('mouseleave', handleMouseLeave);
-  }
+  document.removeEventListener('pointerover', handlePointerOver);
+  document.removeEventListener('pointermove', handlePointerMove);
+  document.removeEventListener('pointerleave', handlePointerLeave);
 });
 </script>
