c2a91da24e977ecea8540271c2b799ca59d8e66d
PixelCamera <abnerpants@gmail.com>
2024-08-10 15:40:45 +0800

    refactor: Update viewport meta tag and calculator styles

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -37,7 +37,7 @@ function switchInfo() {
   <p v-if="showInfo" class="w-full px-8 py-2 text-center">{{ calcState }}</p>
 
   <!--calculator-->
-  <div class="calc-container global-flex flex-col gap-1 p-1">
+  <div class="calc-container global-flex m-4 flex-col gap-1 p-1">
     <!--header-->
     <div class="global-flex w-full gap-4 px-4 py-2">
       <font-awesome-icon icon="fa-solid fa-calculator" />
@@ -69,7 +69,7 @@ function switchInfo() {
     </div>
 
     <!--buttons-->
-    <div class="grid grid-cols-4 gap-1">
+    <div class="grid w-full grid-cols-4 gap-1">
       <button
         v-for="btn in BUTTONS"
         :key="btn.value"
