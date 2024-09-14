65ae1e476246480b3a52ffa25c74b4ab74631663
Sympathy <abnerpants@gmail.com>
2024-09-15 02:18:48 +0800

    refactor: Update package.json and main.js
    Update package.json to include "@vue/language-server" as a devDependency.
    Update main.js to add comments and improve code formatting.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// main.js
 import './main.css';
 
 import { createApp } from 'vue';
@@ -5,10 +6,12 @@ import App from './App.vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // 已包含 'info-circle' 和 'times'
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fas, far, fab);
+library.add(fas, far, fab); // fas 包含所有 free solid 图标
 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
